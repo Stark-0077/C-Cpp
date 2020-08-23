2f351ffc5bee76ac89e265f6b91f73a41c6aea4b
Wolf <u7040076@anu.edu.au>
2020-08-23 20:43:20 +1000

    COMMIT

--- Euler_method.c
+++ Euler_method.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include<math.h>
+#define pai 3.14
 int main()
 {
-    
     int i;
-    float theta = 0, x = 0.42, y = 0;
-    for(i = 1; i<100; i++)
+    float theta = 0, x = 4/(3*pai), y = 0;
+    for(i = 1; i<=100; i++)
     {
-        theta = theta + 0.05;
+        theta = theta + 0.0471;
         x = x + 0.01 * cos(theta);
         y = y + 0.01 * sin(theta);
+        printf("step %d  \t", i );
+        printf("%f %f %f",theta,x,y);
+        printf("\n");
     }
-    printf("%f %f %f",theta,x,y);
     return 0;
 }
