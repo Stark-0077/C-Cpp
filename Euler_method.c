9ed7f5e3e9d466047ab96b928649620c01288d40
Wolf <u7040076@anu.edu.au>
2020-08-24 00:00:07 +1000

    commit

--- Euler_method.c
+++ Euler_method.c
@@ -7,9 +7,9 @@ int main()
     float theta = 0, x = 4/(3*pai), y = 0;
     for(i = 1; i<=100; i++)
     {
-        theta = theta + 0.0471;
         x = x + 0.01 * cos(theta);
         y = y + 0.01 * sin(theta);
+        theta = theta + 0.0471;
         printf("step %d  \t", i );
         printf("%f %f %f",theta,x,y);
         printf("\n");
