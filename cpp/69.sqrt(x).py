8f54cc735f827ca5b7e88bc5222f5224442a719c
TianZhao-007 <u7040076@anu.edu.au>
2020-09-25 15:00:54 +1000

    commit

---
+++ cpp/69.sqrt(x).py
@@ -0,0 +1,24 @@
+# one solution using binary search technique
+
+class Solution {
+public:
+int mySqrt(int x)
+{
+	long long low = 0;
+	long long high = x;
+
+	while (low < high)
+	{
+		long long mid = (high - low) / 2 + low;
+
+		if (mid * mid == x)
+			return mid;
+		else if (mid * mid > x)
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+
+	return low * low > x ? low - 1 : low;
+}
+};
